43909d0240932bf987405f01bda92302992e228d
Mohd Ahsan <106815640+Ahsan117@users.noreply.github.com>
2025-02-24 14:15:52 +0530

    authMiddleware.js

--- auth-service/middleware/authMiddleware.js
+++ auth-service/middleware/authMiddleware.js
@@ -1,15 +1,15 @@
 const jwt = require("jsonwebtoken");
 
-// ✅ Middleware to Protect Routes for Both Customers & Delivery Agents
+
 const protect = async (req, res, next) => {
   let token;
 
   if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
     try {
-      token = req.headers.authorization.split(" ")[1]; // Get Token from Header
-      const decoded = jwt.verify(token, process.env.JWT_SECRET); // Verify Token
+      token = req.headers.authorization.split(" ")[1]; 
+      const decoded = jwt.verify(token, process.env.JWT_SECRET); 
 
-      req.user = decoded.id; // ✅ Store User ID (Works for Both Customers & Agents)
+      req.user = decoded.id; 
       next(); // Move to Next Middleware
     } catch (error) {
       res.status(401).json({ message: "Not authorized, invalid token" });
